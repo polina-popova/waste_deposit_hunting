f2968c1272a554d465b544548fe666b1352be10e
Попова Полина <ppopova.wis@gmail.com>
2019-12-04 17:58:14 +0300

    Deploy to Heroku

---
+++ Procfile
@@ -0,0 +1,2 @@
+release: python manage.py migrate
+web: gunicorn waste_deposit_hunting.wsgi -t 1000
--- waste_deposit_hunting/settings.py
+++ waste_deposit_hunting/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+import django_heroku
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -122,3 +125,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+try:
+    from settings_local import *
+except ImportError:
+    pass
+
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
