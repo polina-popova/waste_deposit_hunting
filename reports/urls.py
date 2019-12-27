1202f81ae73ce70dc774c50284cce8ba0a3c84af
Попова Полина <ppopova.wis@gmail.com>
2019-12-27 07:55:42 +0300

    Add privacy policy endpoint

--- reports/urls.py
+++ reports/urls.py
@@ -1,3 +1,5 @@
+from django.urls import path
+from django.views.generic import TemplateView
 from rest_framework import routers
 
 from .views import ReportViewSet
@@ -7,3 +9,5 @@ router = routers.SimpleRouter()
 router.register(r'reports', ReportViewSet, basename='reports')
 
 urlpatterns = router.urls
+
+urlpatterns.append(path('privacy-policy/', TemplateView.as_view(template_name="privacy_policy.html")),)
