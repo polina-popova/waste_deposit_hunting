caa7d663de431e81991893575ca7d8869edfa1e3
Попова Полина <ppopova.wis@gmail.com>
2020-01-04 14:08:34 +0300

    Add customer agreement

--- reports/urls.py
+++ reports/urls.py
@@ -10,4 +10,7 @@ router.register(r'reports', ReportViewSet, basename='reports')
 
 urlpatterns = router.urls
 
-urlpatterns.append(path('privacy-policy/', TemplateView.as_view(template_name="privacy_policy.html")),)
+urlpatterns += [
+    path('privacy-policy/', TemplateView.as_view(template_name="privacy_policy.html")),
+    path('customer-agreement/', TemplateView.as_view(template_name="customer_agreement.html")),
+]
