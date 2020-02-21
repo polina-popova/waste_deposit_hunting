2879ec5866f035039448564e2cf7ca9699969e3d
Попова Полина <ppopova.wis@gmail.com>
2020-02-21 20:17:36 +0300

    Add email validation in admin site

--- reports/helpers.py
+++ reports/helpers.py
@@ -1,2 +1,10 @@
 def get_desired_width(desired_height, image):
     return round(desired_height * image.width / image.height)
+
+
+def clean_up_emails(emails_string):
+    clean_emails = [
+        letter for letter in emails_string
+        if letter not in ('[', ']', '"')
+    ]
+    return ''.join(clean_emails)
